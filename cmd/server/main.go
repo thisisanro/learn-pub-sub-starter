cf77ad82b9cfedecf526a5ffaa51526c2a421156
thisisanro <antonromanenko.me@gmail.com>
2026-03-31 09:55:51 +0000

    add durable queues to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,6 +25,15 @@ func main() {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
+	gl := routing.GameLogSlug
+	key := gl + ".*"
+
+	_, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilTopic, gl, key, pubsub.Durable)
+	if err != nil {
+		log.Fatalf("could not bind queue: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
 	gamelogic.PrintServerHelp()
 	for {
 		words := gamelogic.GetInput()
